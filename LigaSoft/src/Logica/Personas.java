b55781e04b3a66af0fc8c66b111b2ee590f0fbbf
Angel Trochez <angeltrochez@unah.hn>
2019-03-24 21:41:37 -0700

    Esta es la primera version del Sistema LigaSoft se creo el proyecto en Netbeams IDE 8.2 y se agrega la carpeta del UML version 1

---
+++ LigaSoft/src/Logica/Personas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author Santiago
+ */
+public interface Personas {
+     public abstract void setNombreCompleto(String nombreCompleto);
+     public abstract void setEdad(int edad);
+     public abstract void setSexo(String sexo);
+     
+     public abstract String getNombreCompleto();
+     public abstract int getEdad();
+     public abstract String getSexo();
+    
+}
